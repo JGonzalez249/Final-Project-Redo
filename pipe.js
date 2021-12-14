3e428085f319ed31433837e0a2254e69c183654c
jgonzalez249 <46466482+JGonzalez249@users.noreply.github.com>
2021-12-14 18:01:00 -0500

    updated pipes

--- pipe.js
+++ pipe.js
@@ -4,7 +4,7 @@ class Pipe {
     this.top = random(height / 6, (3 / 4) * height);
     this.bottom = height - (this.top + this.spacing);
     this.x = width;
-    this.w = 30;
+    this.w = 45;
     this.speed = 3;
     this.highlight = false;
     this.playerHighlight = false;
@@ -25,10 +25,12 @@ class Pipe {
     if (player.y < this.top || player.y > height - this.bottom) {
       if (player.x > this.x && player.x < this.x + this.w) {
         player.alive = false;
+        this.playerHighlight = true;
         return true;
       }
     }
     player.alive = true;
+    this.playerHighlight = false;
     return false;
   }
 
@@ -36,6 +38,10 @@ class Pipe {
     fill(255);
     if (this.highlight === true) {
       fill(255, 0, 0);
+    }
+
+    if (this.playerHighlight === true) {
+      fill(0, 0, 255);
     }
     rectMode(CORNER);
     rect(this.x, 0, this.w, this.top);
